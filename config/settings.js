1c8d9063fdcb8c252cfb8bab8651456126e106a2
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-03 15:52:30 +0530

    STRAPI_URL is now taken from env; default is localhost:1337

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,2 @@
-export const STRAPI_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://egt-strapi-db.herokuapp.com'
-    : 'http://localhost:1337';
+export const STRAPI_URL = process.env.STRAPI_URL || "http://localhost:1337";
+
